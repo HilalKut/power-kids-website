5aae8a65d628bb9f24fd5569f70a65403aa151f3
Hilal Kutlu <hilalkutlu@hilal-MacBook-Air.local>
2026-02-20 15:27:13 +0300

    hero section added

---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <section class="hero">
+    <div class="hero-left">
+      <h1>Unlock Your Child's <span>Full Potential</span></h1>
+      <p>
+        Track development milestones, understand behavior patterns
+        and support your child’s emotional growth with data-driven insights.
+      </p>
+      <div class="cta">
+        <button class="primary">Start Tracking</button>
+        <button class="secondary">Learn More</button>
+      </div>
+    </div>
+
+    <div class="hero-right">
+      <div class="mockup"></div>
+    </div>
+  </section>
+`
---
+++ src/style.ss
@@ -0,0 +1,31 @@
+body {
+  margin: 0;
+  font-family: sans-serif;
+}
+
+.hero {
+  height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 80px;
+}
+
+.hero-left {
+  width: 50%;
+}
+
+.hero-left h1 {
+  font-size: 64px;
+}
+
+.hero-left span {
+  color: #30a9e0;
+}
+
+.hero-right .mockup {
+  width: 300px;
+  height: 600px;
+  background: #f68c28;
+  border-radius: 40px;
+}
